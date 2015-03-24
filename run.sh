2a736d4b90ef91468f2e93ecd0e88192c86ca2a8
lasconic <lasconic@gmail.com>
2015-03-24 15:49:08 +0100

    use directory path instead of file path

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cd $ABSPATH
 
 PDFILE=MuseScore-en.pdf
 wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/36546?pdf $PDFILE
-scp $PDFILE musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/$PDFILE
+scp $PDFILE musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/
 ssh musescore@ftp.osuosl.org "~/trigger-musescore"
 rm $PDFILE
