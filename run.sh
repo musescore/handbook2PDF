ab0158abb076470a7bcc7a8a161ab774f1bf5315
lasconic <lasconic@gmail.com>
2015-03-24 15:41:53 +0100

    run from everywhere

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+echo $ABSPATH
+cd $ABSPATH
+
 wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/36546?pdf  MuseScore-en.pdf
 
 scp MuseScore-en.pdf musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/
