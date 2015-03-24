ee767eb7eaa8c17a6255dfbb20d56ea36c628887
lasconic <lasconic@gmail.com>
2015-03-24 15:44:19 +0100

    fix upload

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd)
 echo $ABSPATH
 cd $ABSPATH
 
-wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/36546?pdf  MuseScore-en.pdf
 
-scp MuseScore-en.pdf musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/
+
+PDFILE=MuseScore-en.pdf
+wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/36546?pdf $PDFILE
+scp $PDFILE musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/$PDFILE
 ssh musescore@ftp.osuosl.org "~/trigger-musescore"
+rm $PDFILE
