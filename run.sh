f82356d19a55d95d8ce79ec5ceb2ccf84ae3797e
lasconic <lasconic@gmail.com>
2015-03-24 15:52:06 +0100

    add shebang

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 ABSPATH=$(cd "$(dirname "$0")"; pwd)
 echo $ABSPATH
 cd $ABSPATH
-
-
 
 PDFILE=MuseScore-en.pdf
 wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/36546?pdf $PDFILE
