836579354aa0f2f88d6387a9eef86c2377926e4d
lasconic <lasconic@gmail.com>
2015-03-26 17:11:05 +0100

    use absolute path for wkhtmltopdf

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd)
 echo $ABSPATH
 cd $ABSPATH
 
+WKHTML=/usr/local/bin/wkhtmltopdf
+
 #list of languages and nodes
 LANGUAGES=( en )
 NIDS=( 36546 )
@@ -16,8 +18,9 @@ while [ "$INDEX" -lt "$LANGUAGE_COUNT" ]
 do
     LANGUAGE=${LANGUAGES[$INDEX]}
     NID=${NIDS[$INDEX]}
+    echo "update handbook $LANGUAGE"
     PDFILE=MuseScore-${LANGUAGE}.pdf
-    wkhtmltopdf --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/${NID}?pdf $PDFILE
+    $WKHTML --title "MuseScore 2.0 handbook" cover http://musescore.org/en/handbook-cover toc --xsl-style-sheet custom.xslt http://musescore.org/en/print/book/export/html/${NID}?pdf $PDFILE
     scp $PDFILE musescore@ftp.osuosl.org:~/data/handbook/MuseScore-2.0/
     ssh musescore@ftp.osuosl.org "~/trigger-musescore"
     rm $PDFILE
